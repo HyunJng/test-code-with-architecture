6e4b98fc7200df6baabc671de7e93e0b38441aa6
HyunJng <khjung1654@gmail.com>
2025-06-08 23:48:56 +0900

    UserController 테스트 추가

--- src/test/resources/sql/user-repository-test-data.sql
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -1,4 +1,5 @@
-INSERT INTO `users`(`email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-VALUES('test101@naver.com', 'test101', 'Seoul', 'aaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
-INSERT INTO `users`(`email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-VALUES('test102@naver.com', 'test102', 'Seoul', 'bbbbb-bbbbb-bbbbb', 'PENDING', 0);
+INSERT INTO `users`(`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES(1, 'test101@naver.com', 'test101', 'Seoul', 'aaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
+INSERT INTO `users`(`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES(2, 'test102@naver.com', 'test102', 'Seoul', 'bbbbb-bbbbb-bbbbb', 'PENDING', 0);
+ALTER TABLE users ALTER COLUMN id RESTART WITH 3;
