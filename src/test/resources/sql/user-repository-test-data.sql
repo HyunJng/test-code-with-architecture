c3e9a8209a0156c29a7e5512d1a7433c0e2ea0df
HyunJng <khjung1654@gmail.com>
2025-06-08 16:54:02 +0900

    UserRepository 테스트 추가

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `users`(`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES(1, 'test101@naver.com', 'test101', 'Seoul', 'aaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
+INSERT INTO `users`(`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES(2, 'test102@naver.com', 'test102', 'Seoul', 'bbbbb-bbbbb-bbbbb', 'PENDING', 0);
